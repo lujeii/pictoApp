4efa00a744cf5f76d41c1340d37d428b60217ac7
Fabien BENARIAC <fabien@benariac.fr>
2013-03-01 22:58:08 +0100

    scroll windows set up for gallery display.

--- app/views/galleryView.rb
+++ app/views/galleryView.rb
@@ -1,6 +1,6 @@
 require_relative 'pictoView'
 
-class GalleryView < FXMatrix
+class GalleryView < FXScrollWindow
 
   # attributes set up
   attr_reader :gallery
@@ -9,12 +9,19 @@ class GalleryView < FXMatrix
   def initialize( p, gallery )
     super( p, :opts => LAYOUT_FILL )
     @gallery = gallery
+    FXMatrix.new( self, :opts => LAYOUT_FILL|MATRIX_BY_COLUMNS )
     @gallery.each_picture {|pic| add_pic(pic) }
   end
   
+  # layout
+  def layout
+    contentWindow.numColumns = [width/PictoView::MAX_WIDTH, 1].max
+    super
+  end
+
   # add picto view in this gallery
   def add_pic( pic )
-    PictoView.new( self, pic )
+    PictoView.new( contentWindow, pic )
   end
   
 end
